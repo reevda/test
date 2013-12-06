0cb42fe32d26cd542707e413cbee72007fa0c78d
David Reeves <david.reeves@bluewin.ch>
2013-12-06 18:39:31 +0100

    index.js reset

--- js/index.js
+++ js/index.js
@@ -47,8 +47,8 @@ var app = {
 
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
-        navigator.notification.vibrate(1000);
-//        console.log('Received Event: ' + id);
+
+        console.log('Received Event: ' + id);
     },
 
     scan: function() {
@@ -57,27 +57,25 @@ var app = {
         var scanner = cordova.require("cordova/plugin/BarcodeScanner");
 
         scanner.scan( function (result) { 
-        	navigator.notification.vibrate(500);
-//        	alert('result OK '+result );
-        	alert("We got a barcode\n" + 
+
+            alert("We got a barcode\n" + 
             "Result: " + result.text + "\n" + 
             "Format: " + result.format + "\n" + 
             "Cancelled: " + result.cancelled);  
 
-            /*console.log("Scanner result: \n" +
+           console.log("Scanner result: \n" +
                 "text: " + result.text + "\n" +
                 "format: " + result.format + "\n" +
                 "cancelled: " + result.cancelled + "\n");
             document.getElementById("info").innerHTML = result.text;
             console.log(result);
-            
+            /*
             if (args.format == "QR_CODE") {
                 window.plugins.childBrowser.showWebPage(args.text, { showLocationBar: false });
             }
             */
 
         }, function (error) { 
-        	navigator.notification.vibrate(1500);
             console.log("Scanning failed: ", error); 
         } );
     },
@@ -95,7 +93,3 @@ var app = {
     }
 
 };
-
-function alertDismissed(){
-	alert('alert dismissed!');
-}
