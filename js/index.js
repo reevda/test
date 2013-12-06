037c16c71b6d62fa2d18059497c07e5c72942378
David Reeves <david.reeves@bluewin.ch>
2013-12-06 16:59:22 +0100

    js rm console op

--- js/index.js
+++ js/index.js
@@ -63,13 +63,13 @@ var app = {
             "Format: " + result.format + "\n" + 
             "Cancelled: " + result.cancelled);  
 
-           console.log("Scanner result: \n" +
+            /*console.log("Scanner result: \n" +
                 "text: " + result.text + "\n" +
                 "format: " + result.format + "\n" +
                 "cancelled: " + result.cancelled + "\n");
             document.getElementById("info").innerHTML = result.text;
             console.log(result);
-            /*
+            
             if (args.format == "QR_CODE") {
                 window.plugins.childBrowser.showWebPage(args.text, { showLocationBar: false });
             }
